535c6c78cb1a6e92460f24d2ac4c1bc4f56a1452
Chris Ford <christophertford@gmail.com>
2014-06-03 21:22:24 +0100

    Remove the test directory as its not used.

--- test/leipzig_from_scratch/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns leipzig-from-scratch.core-test
-  (:require [clojure.test :refer :all]
-            [leipzig-from-scratch.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
